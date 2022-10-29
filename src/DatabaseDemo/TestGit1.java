57124e173f18c1281a9f8ff3478344e9fd9356cc
nagaraj1328 <116623032+nagaraj1328@users.noreply.github.com>
2022-10-29 10:43:05 +0530

    Update TestGit1.java

--- src/DatabaseDemo/TestGit1.java
+++ src/DatabaseDemo/TestGit1.java
@@ -3,7 +3,7 @@ package DatabaseDemo;
 public class TestGit1 {
 
 	public static void main(String[] args) {
-		System.out.println("Git Push and Pull");
+		System.out.println("Git PUSH and Pull");
 
 	}
 
